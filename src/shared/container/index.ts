28feabaaddbd182bb4d158bd6cc813187a73ba12
Myllena Cruz <myllena.ocruz@gmail.com>
2023-02-07 20:44:47 -0300

    register user singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,3 +1,8 @@
 import { container } from "tsyringe";
 
 import "@shared/container/providers";
+
+import { IUserRepository } from "@modules/user/repositories/IUserRepository";
+import { FakeUserRepository } from "@modules/user/repositories/implementations/FakeUserRepository";
+
+container.registerSingleton<IUserRepository>("UserRepository", FakeUserRepository);
