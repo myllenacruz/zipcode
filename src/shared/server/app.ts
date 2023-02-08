e8595a6f6e858c1361a9ca8b8ff0eba00b90a361
Myllena Cruz <myllena.ocruz@gmail.com>
2023-02-07 21:48:40 -0300

    add global exception handler middleware

--- src/shared/server/app.ts
+++ src/shared/server/app.ts
@@ -1,12 +1,14 @@
 import "reflect-metadata";
 import "dotenv/config";
 import "@shared/container";
+import "express-async-errors";
 
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { routes } from "@shared/server/routes";
 import { Server as HttpServer } from "http";
 import cors from "cors";
 import { errors } from "celebrate";
+import { AppError } from "@shared/errors/AppError";
 
 const app = express();
 const server = new HttpServer(app);
@@ -17,5 +19,23 @@ app.use(cors());
 app.use(routes);
 
 app.use(errors());
+
+app.use((
+	error: Error,
+	_req: Request,
+	res: Response,
+	_next: NextFunction
+) => {
+	if (error instanceof AppError) {
+		return res.status(error.statusCode).json({
+			message: error.message
+		});
+	}
+
+	console.error(error);
+	return res.status(500).json({
+		message: "Internal Server Error"
+	});
+});
 
 export { server };
