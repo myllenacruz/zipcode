e36c4f82e29108163e5c2809502c5de0048c8150
Myllena Cruz <myllena.ocruz@gmail.com>
2023-02-07 21:53:19 -0300

    add auth middleware in zip code consult route

--- src/modules/zipCode/routes/zipCode.routes.ts
+++ src/modules/zipCode/routes/zipCode.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { ZipCodeController } from "@modules/zipCode/controllers/ZipCodeController";
 import { celebrate, Segments, Joi } from "celebrate";
+import { ensureAuthentication } from "@modules/user/middlewares/ensureAuthentication";
 
 const routes = Router();
 const zipCodeController = new ZipCodeController();
 
 routes.post("/",
+	ensureAuthentication,
 	celebrate({
 		[Segments.BODY]: {
 			zipCode: Joi.string().required().min(8).max(8)
